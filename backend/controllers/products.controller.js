9b7fc604df5eba33a40d96a63c97c27751305160
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-12-15 17:26:14 +0300

    Initial

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -39,12 +39,16 @@ export const createProduct = async(req, res) =>{
 export const deleteProduct = async(req, res) =>{
     const { id } = req.params; 
 
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid product id" });
+    }
+
     try {
         await Product.findByIdAndDelete(id);
         res.status(200).json({success: true, message: "Product deleted"});
     }
     catch (error){
-        res.status(404).json({ success: false, message: "Product not found"});
+        res.status(500).json({ success: false, message: "Product not in system"});
     }
 };
 
--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,14 @@ dotenv.config();
 // Creating an instance of express
 const app = express();
 
+const PORT = process.env.PORT || 5000
+
 app.use(express.json());
 
 app.use("/api/products",productRoutes);
 
 // Setting up the port our site will run on
-app.listen( 5000, () => {
+app.listen( PORT, () => {
     connectDB(); // connecting to the database
-    console.log('Server started at http://localhost:5000');
-})
+    console.log('Server started at http://localhost:' + PORT);
+});
