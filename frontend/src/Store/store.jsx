c24fe60d6ddbe23c605eae6c8557c8a1d78e4e0b
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2024-12-17 14:29:10 +0300

    Initial

--- frontend/src/Store/store.jsx
+++ frontend/src/Store/store.jsx
@@ -1,9 +1,31 @@
-
 import { create } from "zustand";
 
+
 export const useStore = create((set) => ({
-    // your state here
-    products: [],
-    // your actions here
-    setProducts: (products) => set({ products }),
+    // Add your state and actions here
+	products: [],
+	setProducts: (products) => set({ products }),
+
+	createProduct: async (newProduct) => {
+        if (!newProduct.name) {
+            return { success: false, message: "Product name is required and must be a string." };
+        }
+        if (!newProduct.price) {
+            return { success: false, message: "Price is required and must be a number." };
+        }
+        if (!newProduct.image) {
+            return { success: false, message: "Image is required and must be a string." };
+        }
+
+		const res = await fetch("/api/products", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newProduct),
+		});
+		const data = await res.json();
+		set((state) => ({ products: [...state.products, data.data] }));
+        return { success: true, message: "Product created successfully." };
+	},
 }));
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,67 +1,102 @@
-import { Box, Container, Heading, VStack, Button, Input } from '@chakra-ui/react';
-import React, { useState } from 'react'
+import {
+	Box,
+	Container,
+	Heading,
+	VStack,
+	Button,
+	Input
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useStore } from "../Store/store";
+import { toaster } from "@/components/ui/toaster"
 
 function CreatePage() {
 
-    const [newProduct, SetnewProduct] = useState({
-        Product_name: "",
-        price: "",
-        image: "",
-    });
+	const [newProduct, SetnewProduct] = useState({
+		name: "",
+		price: "",
+		image: "",
+	});
 
-    const handleAddProduct = () => {
-        console.log(newProduct);
-    }
+    const {createProduct} = useStore()
 
-    return (
-        <Container maxW={"container.sm"}>
-            <VStack spaceY={8}>
+	const handleAddProduct = async () => {
+        console.log(newProduct)
+        const { success, message } = await createProduct(newProduct);
 
-                <Heading as={"h1"} size={"4xl"} textAlign={"center"} mb={8} fontFamily={"revert"} color="Highlight" fontWeight={"bold"}>
-                    Create New Product
-                </Heading>
-                <Box
-                    width={"full"}
-                    bg={"gray.800"}
-                    p={6} rounded={"lg"} shadow={"md"}
-                >
-                    <VStack spaceY={4}>
-                    
-                        <Input
-                          placeholder = "Product Name"
-                          border= "0.5px solid"
-                          borderColor="#696969"
-                          name = "Product_name"
-                          value = {newProduct.Product_name}
-                          onChange = {(e) => SetnewProduct({...newProduct, Product_name: e.target.value})}
-                        />
+        console.log("Success:", success);
+        console.log("Message:", message);
+        
+	};
 
-                        <Input
-                          placeholder = "Price"
-                          border= "0.5px solid"
-                          borderColor="#696969"
-                          name = "price"
-                          type='number'
-                          value = {newProduct.price}
-                          onChange = {(e) => SetnewProduct({...newProduct, price: e.target.value})}
-                        />
+	return (
+		<Container maxW={"container.sm"}>
+			<VStack spaceY={8}>
+				<Heading
+					as={"h1"}
+					size={"4xl"}
+					textAlign={"center"}
+					mb={8}
+					fontFamily={"revert"}
+					color='Highlight'
+					fontWeight={"bold"}>
+					Create New Product
+				</Heading>
+				<Box width={"full"} bg={"gray.800"} p={6} rounded={"lg"} shadow={"md"}>
+					<VStack spaceY={4}>
 
-                        <Input
-                          placeholder = "Image URL"
-                          border= "0.5px solid"
-                          borderColor="#696969"
-                          name = "image"
-                          value = {newProduct.image}
-                          onChange = {(e) => SetnewProduct({...newProduct, image: e.target.value})}
-                        />
+						<Input
+							placeholder='Product Name'
+							border='0.5px solid'
+							borderColor='#696969'
+                            required
+							name='name'
+							value={newProduct.name}
+							onChange={(e) =>
+								SetnewProduct({ ...newProduct, name: e.target.value })
+							}
+						/>
 
-                        <Button bgGradient= "to-r" gradientFrom={"#4169E1"} gradientTo={"#B0E0E6"} onClick = {handleAddProduct} w="full" fontSize={"xl"}>Add Product</Button>
-                    </VStack>
-                </Box>
+						<Input
+							placeholder='Price'
+							border='0.5px solid'
+							borderColor='#696969'
+                            required
+							name='price'
+							type='number'
+							value={newProduct.price}
+							onChange={(e) =>
+								SetnewProduct({ ...newProduct, price: e.target.value })
+							}
+						/>
 
-            </VStack>
-        </Container>
-    );
+						<Input
+							placeholder='Image URL'
+							border='0.5px solid'
+							borderColor='#696969'
+                            required
+							name='image'
+							value={newProduct.image}
+							onChange={(e) =>
+								SetnewProduct({ ...newProduct, image: e.target.value })
+							}
+						/>
+
+						<Button
+							bgGradient='to-r'
+							gradientFrom={"#4169E1"}
+							gradientTo={"#B0E0E6"}
+							onClick={handleAddProduct}
+							w='full'
+							fontSize={"xl"}>
+							Add Product
+						</Button>
+
+					</VStack>
+				</Box>
+			</VStack>
+		</Container>
+	);
 }
 
-export default CreatePage
+export default CreatePage;
